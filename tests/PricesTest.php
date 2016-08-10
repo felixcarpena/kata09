89da5c5b14da767a8d8123ca817da642fee2f260
Félix Carpena <felix@carpena.eu>
2016-08-10 23:40:47 +0200

    Change old test case reference to the modern namespace

--- tests/PricesTest.php
+++ tests/PricesTest.php
@@ -1,7 +1,9 @@
 <?php
 declare(strict_types = 1);
 
-class PricesTest extends \PHPUnit_Framework_TestCase
+use PHPUnit\Framework\TestCase;
+
+class PricesTest extends TestCase
 {
     /** @test */
     public function checkout() :void
