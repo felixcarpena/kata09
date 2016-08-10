49cb7fc04c437d8431bcf7838de7071822477842
Félix Carpena <felix@carpena.eu>
2016-08-10 20:47:13 +0200

    Basic structure for "Back to the Checkout" kata09.

---
+++ readme.md
@@ -0,0 +1,4 @@
+# Kata09: Back to the Checkout
+It is just a simple implementation of the kata: http://codekata.com/kata/kata16-business-rules/ in php 7.1 with https://github.com/php-ds/extension/tree/master
+## Why?
+Because "modern" php is not that bad ;)
---
+++ tests/PricesTest.php
@@ -0,0 +1,57 @@
+<?php
+declare(strict_types = 1);
+
+class PricesTest extends \PHPUnit_Framework_TestCase
+{
+    /** @test */
+    public function checkout() :void
+    {
+        $this->assertEquals(0, $this->price(""));
+        $this->assertEquals(50, $this->price("A"));
+        $this->assertEquals(80, $this->price("AB"));
+        $this->assertEquals(115, $this->price("CDBA"));
+
+        $this->assertEquals(100, $this->price("AA"));
+        $this->assertEquals(130, $this->price("AAA"));
+        $this->assertEquals(180, $this->price("AAAA"));
+        $this->assertEquals(230, $this->price("AAAAA"));
+        $this->assertEquals(260, $this->price("AAAAAA"));
+
+        $this->assertEquals(160, $this->price("AAAB"));
+        $this->assertEquals(175, $this->price("AAABB"));
+        $this->assertEquals(190, $this->price("AAABBD"));
+        $this->assertEquals(190, $this->price("DABABA"));
+    }
+
+    /** @test */
+    public function checkout_incremental(): void
+    {
+        $checkout = new Cehckout(Rules::create());
+        $this->assertEquals(0, $checkout->total());
+
+        $checkout->scan(new Item("A"));
+        $this->assertEquals(50, $checkout->total());
+
+        $checkout->scan(new Item("B"));
+        $this->assertEquals(80, $checkout->total());
+
+        $checkout->scan(new Item("A"));
+        $this->assertEquals(130, $checkout->total());
+
+        $checkout->scan(new Item("A"));
+        $this->assertEquals(160, $checkout->total());
+
+        $checkout->scan(new Item("B"));
+        $this->assertEquals(175, $checkout->total());
+    }
+
+    //---[ Helpers ]--------------------------------------------------------------------------------------------------//
+    private function price(string $price): int
+    {
+        $checkout = new Cehckout(Rules::create());
+        foreach (str_split($price) as $itemName) {
+            $checkout->scan(new Item($itemName));
+        }
+        return $checkout->total();
+    }
+}
