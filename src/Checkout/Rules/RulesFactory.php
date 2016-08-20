e7cac074c7ee868dee03d401240efbf34a173339
Félix Carpena <felix@carpena.eu>
2016-08-20 23:50:27 +0200

    Set all the rules as the test requiring to be on green

--- src/Checkout/Rules/RulesFactory.php
+++ src/Checkout/Rules/RulesFactory.php
@@ -12,10 +12,15 @@ final class RulesFactory
      */
     public static function create()
     {
+        $itemA = new Item('A');
+        $itemB = new Item('B');
+
         return
             (new Rules())
-                ->add(new Item('A'), new UnitaryRule(50))
-                ->add(new Item('B'), new UnitaryRule(30))
+                ->add($itemA, new DiscountOverUnitiesRule(3, 130))
+                ->add($itemA, new UnitaryRule(50))
+                ->add($itemB, new DiscountOverUnitiesRule(2, 45))
+                ->add($itemB, new UnitaryRule(30))
                 ->add(new Item('C'), new UnitaryRule(20))
                 ->add(new Item('D'), new UnitaryRule(15));
     }
